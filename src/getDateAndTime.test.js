e3d279983bd847fdd472b4af0e0689bb3d4e224a
Stephen Ramsay <rabrandom@gmail.com>
2020-03-31 10:20:31 +0100

    added md5 key generator

--- src/getDateAndTime.test.js
+++ src/getDateAndTime.test.js
@@ -1,5 +1,5 @@
 import getDateInBusTrackerFormat from './getDateAndTime.js'
-import appendDateToKey from './generateApiKey.js'
+import {appendDateToKey, getFinalKeyFromDate} from './generateApiKey.js'
 
 test('it can retrive the current date in Bus Tracker Format', () => {
     const busTrackerFormattedDate = getDateInBusTrackerFormat(
@@ -22,6 +22,9 @@ test('it can append the date to the end of the API key', () => {
     expect(keyPlusDate).toBe('XL472I2YRVUQF2EJ5T77AEENJ2020112914')
 })
 
-// test('it can convert the key into MD5 or something...?', () => {
-//     expect().toBe('12b288629403f5ff0a380e5452e3bfee')
-// })
+test('it can convert the key into MD5 or something...?', () => {
+    const finalKey = getFinalKeyFromDate(
+        new Date('2020-11-29T14:52:51.03Z')
+    )
+    expect(finalKey).toBe('12b288629403f5ff0a380e5452e3bfee')
+})
