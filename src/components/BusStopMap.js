f14e5fabe6dd6c6391267573fe764d78b3b4b0a4
Stephen Ramsay <rabrandom@gmail.com>
2020-04-07 18:19:00 +0100

    added bus numbers to stops

--- src/components/BusStopMap.js
+++ src/components/BusStopMap.js
@@ -1,26 +1,37 @@
 import React from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
-function BusStopMap( {stops} ) {
+
+
+function BusStopMap({ stops, busNumberLookup }) {
+
+    function getCommaSeparatedBusNumbersFromServices(services) {
+        let busNumbersString = ''
+        for (const service of services) {
+            busNumbersString += busNumberLookup[service] + ", "
+        }
+        return busNumbersString.slice(0, busNumbersString.length - 2)
+    }
 
     const markers = stops.map(stop => {
-    return (
-        <Marker 
-            position={ [stop.x, stop.y]}
-            key={stop.stopId}
-        >
-            <Popup>
-                Stop name: {stop.name}
-                Busses: {stop.services}
-            </Popup>
-        </Marker>
-    )
+        return (
+            <Marker
+                position={[stop.x, stop.y]}
+                key={stop.stopId}
+            >
+                <Popup>
+                    Stop name: {stop.name}
+                    <br></br>
+                    Busses: {getCommaSeparatedBusNumbersFromServices(stop.services)}
+                </Popup>
+            </Marker>
+        )
     });
 
     const position = [55.948714, -3.199913]
 
     const map = (
-        <Map center={position} zoom={11.5} style={{height: '500px'}}>
+        <Map center={position} zoom={11.5} style={{ height: '500px' }}>
             <TileLayer
                 url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png"
                 attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://carto.com/attributions">CARTO</a>'
