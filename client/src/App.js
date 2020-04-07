3a4cb87fbd02bab724f930fb1d2f073acfa6e9fb
Stephen Ramsay <rabrandom@gmail.com>
2020-04-07 17:32:22 +0100

    made a new route for bus service number and made a request in the client using useState hook

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,18 @@ import BusStopMap from './components/BusStopMap.js'
 function App() {
 
   const [stops, setStops] = useState([]);
+  const [busNumberKeyLookup, setBusNumberKeyLookup] = useState({});
 
   useEffect(() => {
     fetch('http://localhost:8080')
       .then(res => res.json())
       .then(obJOfStops => setStops(obJOfStops.busStops))
+  }, [])
+
+  useEffect(() => {
+    fetch('http://localhost:8080/busServiceNumbers')
+      .then(res => res.json())
+      .then(busNumberKeyLookup => setBusNumberKeyLookup(busNumberKeyLookup))
   }, [])
 
   return (
--- index.js
+++ index.js
@@ -16,6 +16,26 @@ app.get('/', (request, response) => {
     .then(stops => response.json(stops))
 })
 
+app.get('/busServiceNumbers', (request, response) => {
+  const URL = 'http://ws.mybustracker.co.uk/?module=json&key='
+  + getFinalKeyFromDate(new Date()) + '&function=getServices'
+  fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+      // [{ref: "2993", mnemo: "44"}]
+      // busNumberLookup = {"2993": "44"...}
+      // busNumberLookup["2993"] => 44
+      const finalBusNumberLookup = data.services.reduce((busNumberLookup, service) => {
+        busNumberLookup[service.ref] = service.mnemo
+        return busNumberLookup
+      }, {})
+      return finalBusNumberLookup
+    })
+    .then(finalBusNumberLookup => response.json(finalBusNumberLookup))
+})
+
+
+
 app.listen(8080, () => {
   console.log('listening on port 8080')
 })
